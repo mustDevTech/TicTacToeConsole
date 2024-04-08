3f2ad8d1889cb3c2061420f00ed1223aeba94da1
mustgamedev <mustafashahdzahan@gmail.com>
2024-04-08 18:51:49 +0300

    Improved AI mode. Small board Ui changes. Ability to change board size. Small code refactoring.

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,49 +1,53 @@
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 /**
  * Class to represent the Tic Tac Toe game
  */
-public class TicTacToe
-{
-    private final char[][] board; // 2D array to represent the Tic Tac Toe board
-    private char currentPlayer; // current player, can be X or O
-    private static final char PLAYER_X = 'X'; // constant for player X
-    private static final char PLAYER_O = 'O'; // constant for player O
-    private static final char EMPTY_CELL = '-'; // constant for empty cells on the board
+public class TicTacToe {
+    //region Constants for player symbols
+    private static final char PLAYER_X = 'X';
+    private static final char PLAYER_O = 'O';
+    private static final char EMPTY_CELL = '-';
+    //endregion
+    private static final int boardSize = 3;
+    private final char[][] board;
+    private char currentPlayer;
+
 
     /**
      * Constructor method to initialize the board and current player
      */
-    public TicTacToe()
-    {
-        board = new char[3][3];
-        currentPlayer = PLAYER_X;
+    public TicTacToe() {
+        this.board = new char[boardSize][boardSize];
+        this.currentPlayer = PLAYER_X;
         initializeBoard();
     }
 
+
     /**
-     * Method to initialize the Tic Tac Toe board with empty cells
+     * Method to initialize the board with empty cells
      */
-    private void initializeBoard()
-    {
-        for (int i = 0; i < 3; i++)
-        {
-            Arrays.fill(board[i], EMPTY_CELL);
+    private void initializeBoard() {
+        for (char[] chars : board) {
+            Arrays.fill(chars, EMPTY_CELL);
         }
     }
 
     /**
      * Method to 'Visually' print the Tic Tac Toe board
      */
-    private void printBoard()
-    {
-        System.out.println("  0 1 2");
-        for (int i = 0; i < 3; i++)
-        {
+    private void printBoard() {
+        System.out.print("   ");
+        for (int i = 0; i < board.length; i++) {
             System.out.print(i + " ");
-            for (int j = 0; j < 3; j++)
-            {
+        }
+        System.out.println();
+
+        for (int i = 0; i < board.length; i++) {
+            System.out.print(i + "| ");
+            for (int j = 0; j < board.length; j++) {
                 System.out.print(board[i][j] + " ");
             }
             System.out.println();
@@ -54,58 +58,48 @@ public class TicTacToe
      * Method to check if there is a winner by checking the rows, columns, and diagonals
      * @return true if there is a winner, false otherwise
      */
-    private boolean checkWin()
-    {
-        return checkRows() || checkColumns() || checkDiagonals();
-    }
+    private boolean checkWin() {
+        int boardSize = board.length;
 
-    /**
-     * Method to check if there is a winner in the rows
-     * @return true if there is a winner in the rows, false otherwise
-     */
-    private boolean checkRows()
-    {
-        for (int i = 0; i < 3; i++)
-        {
-            if (board[i][0] == board[i][1] && board[i][0] == board[i][2] && board[i][0] != EMPTY_CELL)
-            {
-                return true;
+        // Check rows and columns simultaneously
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j <= boardSize - 3; j++) {
+                boolean rowWin = true;
+                boolean colWin = true;
+                for (int k = 1; k < 3; k++) {
+                    if (board[i][j] == EMPTY_CELL || board[i][j] != board[i][j + k]) {
+                        rowWin = false;
+                    }
+                    if (board[j][i] == EMPTY_CELL || board[j][i] != board[j + k][i]) {
+                        colWin = false;
+                    }
+                }
+                if (rowWin || colWin) {
+                    return true;
+                }
             }
         }
-        return false;
-    }
 
-    /**
-     * Method to check if there is a winner in the columns
-     * @return true if there is a winner in the columns, false otherwise
-     */
-    private boolean checkColumns()
-    {
-        for (int i = 0; i < 3; i++)
-        {
-            if (board[0][i] == board[1][i] && board[0][i] == board[2][i] && board[0][i] != EMPTY_CELL)
-            {
-                return true;
+        // Check diagonals
+        for (int i = 0; i <= boardSize - 3; i++) {
+            for (int j = 0; j <= boardSize - 3; j++) {
+                boolean mainDiagonalWin = true;
+                boolean otherDiagonalWin = true;
+                for (int k = 1; k < 3; k++) {
+                    if (board[i][j] == EMPTY_CELL || board[i][j] != board[i + k][j + k]) {
+                        mainDiagonalWin = false;
+                    }
+                    if (board[i][j + 2] == EMPTY_CELL || board[i][j + 2] != board[i + k][j + 2 - k]) {
+                        otherDiagonalWin = false;
+                    }
+                }
+                if (mainDiagonalWin || otherDiagonalWin) {
+                    return true;
+                }
             }
         }
-        return false;
-    }
 
-    /**
-     * Method to check if there is a winner in the diagonals
-     * @return true if there is a winner in the diagonals, false otherwise
-     */
-    private boolean checkDiagonals()
-    {
-        if (board[0][0] == board[1][1] && board[0][0] == board[2][2] && board[0][0] != EMPTY_CELL)
-        {
-            return true;
-        }
-        if (board[0][2] == board[1][1] && board[0][2] == board[2][0] && board[0][2] != EMPTY_CELL)
-        {
-            return true;
-        }
-        return false;
+        return false; // No winner found
     }
 
     /**
@@ -113,20 +107,11 @@ public class TicTacToe
      * @param row the row to make the move in
      * @param col the column to make the move in
      */
-    private void makeMove(int row, int col)
-    {
-        if (board[row][col] == EMPTY_CELL)
-        {
+    private void makeMove(int row, int col) {
+        if (board[row][col] == EMPTY_CELL) {
             board[row][col] = currentPlayer;
-            if (currentPlayer == PLAYER_X)
-            {
-                currentPlayer = PLAYER_O;
-            } else
-            {
-                currentPlayer = PLAYER_X;
-            }
-        } else
-        {
+            switchPlayer();
+        } else {
             System.out.println(LanguageManager.getResource("occupiedCell"));
         }
     }
@@ -134,97 +119,99 @@ public class TicTacToe
     /**
      * Method to make a move on the board for the computer AI
      */
-    private void makeAIMove()
-    {
+    private void makeAIMove() {
+        // Check for winning move
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                if (board[i][j] == EMPTY_CELL) {
+                    board[i][j] = currentPlayer;
+                    if (checkWin()) {
+                        switchPlayer();
+                        return;
+                    } else {
+                        board[i][j] = EMPTY_CELL;
+                    }
+                }
+            }
+        }
+
+        // Check for blocking opponent's winning move
+        char opponentPlayer = (currentPlayer == PLAYER_X) ? PLAYER_O : PLAYER_X;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                if (board[i][j] == EMPTY_CELL) {
+                    board[i][j] = opponentPlayer;
+                    if (checkWin()) {
+                        board[i][j] = currentPlayer;
+                        switchPlayer();
+                        return;
+                    } else {
+                        board[i][j] = EMPTY_CELL;
+                    }
+                }
+            }
+        }
+
+        // If no winning or blocking move, make a random move
+        Random random = new Random();
         int row, col;
-        do
-        {
-            row = (int) (Math.random() * 3);
-            col = (int) (Math.random() * 3);
+        do {
+            row = random.nextInt(board.length);
+            col = random.nextInt(board.length);
         } while (board[row][col] != EMPTY_CELL);
         board[row][col] = currentPlayer;
-        if (currentPlayer == PLAYER_X)
-        {
-            currentPlayer = PLAYER_O;
-        } else
-        {
-            currentPlayer = PLAYER_X;
-        }
+        switchPlayer();
     }
 
     /**
      * Method to play the Tic Tac Toe game against the computer AI
      */
-    public void play()
-    {
+    public void playWithAI() {
         Scanner scanner = new Scanner(System.in);
-        while (!checkWin())
-        {
+        while (!checkWin()) {
             printBoard();
-            if (currentPlayer == PLAYER_X)
-            {
+            if (currentPlayer == PLAYER_X) {
                 System.out.println(LanguageManager.getResource("playerTurnRow", currentPlayer) + " ");
                 int row = scanner.nextInt();
                 System.out.println(LanguageManager.getResource("playerTurnColumn", currentPlayer) + " ");
                 int col = scanner.nextInt();
                 makeMove(row, col);
-            } else
-            {
+            } else {
                 System.out.println(LanguageManager.getResource("computersTurn"));
                 makeAIMove();
             }
         }
         printBoard();
-        if (currentPlayer == PLAYER_X)
-        {
+        if (currentPlayer == PLAYER_X) {
             System.out.println(LanguageManager.getResource("playerOWins"));
-        } else
-        {
+        } else {
             System.out.println(LanguageManager.getResource("playerXWins"));
         }
         scanner.close();
     }
 
     /**
-     * Method to make a move on the board for the current player in a two-player game
-     * @param row the row to make the move in
-     * @param col the column to make the move in
-     */
-    private void makeMoveWithFriend(int row, int col)
-    {
-        if (board[row][col] == EMPTY_CELL)
-        {
-            board[row][col] = currentPlayer;
-            if (currentPlayer == PLAYER_X)
-            {
-                currentPlayer = PLAYER_O;
-            } else
-            {
-                currentPlayer = PLAYER_X;
-            }
-        } else
-        {
-            System.out.println(LanguageManager.getResource("occupiedCell"));
-        }
-    }
-
-    /**
      * Method to play the Tic Tac Toe game with a friend
      */
-    public void playWithFriend()
-    {
+    public void playWithFriend() {
         Scanner scanner = new Scanner(System.in);
-        while (!checkWin())
-        {
+        while (!checkWin()) {
             printBoard();
             System.out.print(LanguageManager.getResource("playerTurnRow", currentPlayer) + " ");
             int row = scanner.nextInt();
             System.out.print(LanguageManager.getResource("playerTurnColumn", currentPlayer) + " ");
             int col = scanner.nextInt();
-            makeMoveWithFriend(row, col);
+            makeMove(row, col);
         }
         printBoard();
         System.out.print(LanguageManager.getResource("playerWins", currentPlayer));
         scanner.close();
+    }
+
+    /**
+     * Method to switch the current player
+     */
+    private void switchPlayer() {
+        currentPlayer = (currentPlayer == PLAYER_X) ? PLAYER_O : PLAYER_X;
     }
 }
